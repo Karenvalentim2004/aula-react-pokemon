b1c10c64beb154c8cc39c82ab5dcc93327783c3a
Karen Valentim <karenvalentim54@gmail.com>
2025-07-31 20:16:59 -0300

    Css
    criação do css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <>
-      
+      <div className="card">
+        <h2>Pokemon</h2>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+      </div>
     </>
   )
 }
