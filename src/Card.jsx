227a353bd2a1f722e3a6d4aacd0d1649e58f9035
Karen Valentim <karenvalentim54@gmail.com>
2025-07-31 22:26:35 -0300

    Adicionando imagem e alteração css

---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Card.css'
+
+function Card({ info }) {
+  return (
+    <div className="card">
+      <img src={info.foto} alt={info.nome} />
+      <div>{info.numero}</div>
+      <div>{info.nome}</div>
+      <div>{info.tipo}</div>
+    </div>
+  )
+}
+ 
+
+export default Card
